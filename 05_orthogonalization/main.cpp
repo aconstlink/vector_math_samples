e1605912da58748651f62a3cde507d5f304780b4
aconstlink <aconstlink@gmail.com>
2023-09-11 17:02:25 +0200

    :tada: Initialize with starter application :sparkles:

---
+++ 05_orthogonalization/main.cpp
@@ -0,0 +1,177 @@
+
+
+#include <natus/application/global.h>
+#include <natus/application/app.h>
+#include <natus/application/util/app_essentials.h>
+#include <natus/tool/imgui/custom_widgets.h>
+
+#include <natus/format/global.h>
+#include <natus/format/future_items.hpp>
+#include <natus/gfx/sprite/sprite_render_2d.h>
+
+#include <natus/profile/macros.h>
+
+#include <natus/physics/particle_system.h>
+#include <natus/physics/force_fields.hpp>
+
+#include <natus/collide/2d/bounds/aabb.hpp>
+#include <natus/collide/2d/bounds/circle.hpp>
+#include <natus/collide/2d/hit_tests.hpp>
+
+#include <random>
+#include <thread>
+
+namespace this_file
+{
+    using namespace natus::core ;
+    using namespace natus::core::types ;
+
+    //****************************************************************************************
+    //
+    //
+    class test_app : public natus::application::app
+    {
+        natus_this_typedefs( test_app ) ;
+
+    private:
+
+        natus::application::util::app_essentials_t _ae ;
+
+    private:        
+
+    public:
+
+        test_app( void_t ) noexcept
+        {
+            natus::application::app::window_info_t wi ;
+            #if 1
+            auto view1 = this_t::create_window( "A Render Window Default", wi ) ;
+            auto view2 = this_t::create_window( "A Render Window Additional", wi,
+                { natus::graphics::backend_type::gl4, natus::graphics::backend_type::d3d11}) ;
+
+            view1.window().position( 50, 50 ) ;
+            view1.window().resize( 800, 800 ) ;
+            view2.window().position( 50 + 800, 50 ) ;
+            view2.window().resize( 800, 800 ) ;
+
+            _ae = natus::application::util::app_essentials_t( 
+                natus::graphics::async_views_t( { view1.async(), view2.async() } ) ) ;
+            #else
+            auto view1 = this_t::create_window( "A Render Window", wi, 
+                { natus::graphics::backend_type::gl4, natus::graphics::backend_type::d3d11 } ) ;
+            _ae = natus::application::util::app_essentials_t( 
+                natus::graphics::async_views_t( { view1.async() } ) ) ;
+            #endif
+        }
+        test_app( this_cref_t ) = delete ;
+        test_app( this_rref_t rhv ) noexcept : app( ::std::move( rhv ) ) 
+        {
+            _ae = std::move( rhv._ae ) ;
+        }
+        virtual ~test_app( void_t ) noexcept {}
+
+        virtual natus::application::result on_event( window_id_t const wid, this_t::window_event_info_in_t wei ) noexcept
+        {
+            _ae.on_event( wid, wei ) ;
+            _ae.get_camera_0()->orthographic() ;
+            return natus::application::result::ok ;
+        }
+
+    private:
+
+        virtual natus::application::result on_init( void_t ) noexcept
+        { 
+            natus::application::util::app_essentials_t::init_struct is = 
+            {
+                { "myapp" }, 
+                { natus::io::path_t( DATAPATH ), "./working", "data" }
+            } ;
+
+            _ae.init( is ) ;
+            _ae.enable_mouse_control( false ) ;
+
+            return natus::application::result::ok ; 
+        }
+
+        virtual natus::application::result on_device( device_data_in_t dd ) noexcept 
+        { 
+            _ae.on_device( dd ) ;
+
+            // mouse
+            {
+                natus::device::layouts::three_mouse_t mouse( _ae.get_mouse_dev() ) ;
+
+                if( mouse.is_pressing( natus::device::layouts::three_mouse::button::right ) )
+                {
+                }
+
+                if( mouse.is_pressing( natus::device::layouts::three_mouse::button::left ) )
+                {
+                }
+            }
+
+            // keyboard
+            {
+                natus::device::layouts::ascii_keyboard_t keyboard( _ae.get_ascii_dev() ) ;
+                
+                if( keyboard.get_state( natus::device::layouts::ascii_keyboard::ascii_key::space ) == 
+                    natus::device::components::key_state::pressing )
+                {
+                }
+                else if( keyboard.get_state( natus::device::layouts::ascii_keyboard::ascii_key::space ) == 
+                    natus::device::components::key_state::released )
+                {
+                }
+            }
+
+            return natus::application::result::ok ; 
+        }
+
+        virtual natus::application::result on_update( natus::application::app_t::update_data_in_t ud ) noexcept 
+        { 
+            return natus::application::result::ok ; 
+        }
+
+        virtual natus::application::result on_physics( natus::application::app_t::physics_data_in_t ud ) noexcept
+        {
+            return natus::application::result::ok ; 
+        }
+
+        //***************************************************************************************************************
+        virtual natus::application::result on_graphics( natus::application::app_t::render_data_in_t rd ) noexcept 
+        {
+            _ae.on_graphics_begin( rd ) ;
+
+            auto pr = _ae.get_prim_render() ;
+
+            
+
+            _ae.on_graphics_end( 100 ) ;
+
+            return natus::application::result::ok ; 
+        }
+
+        virtual natus::application::result on_tool( natus::application::app::tool_data_ref_t td ) noexcept
+        {
+            if( !_ae.on_tool( td ) ) return natus::application::result::ok ;
+
+            ImGui::Begin( "Control" ) ;
+            {
+               
+            }
+            ImGui::End() ;
+
+            return natus::application::result::ok ;
+        }
+
+        virtual natus::application::result on_shutdown( void_t ) noexcept 
+        { return natus::application::result::ok ; }
+    };
+    natus_res_typedef( test_app ) ;
+}
+
+int main( int argc, char ** argv )
+{
+    return natus::application::global_t::create_and_exec_application( 
+        this_file::test_app_res_t( this_file::test_app_t() ) ) ;
+}
